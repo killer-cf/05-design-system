b8cab6e542bf3796e0e8602032600af928b9772b
killer-cf <costa.kilder@live.com>
2023-03-24 09:08:56 -0300

    feat: component Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@killercf-ui/react'
+
+export default {
+  title: 'Typogra/Text',
+  component: Text,
+  args: {
+    children: 'exemplo de textto para o storybook',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'strong text',
+    as: 'strong',
+  },
+}
