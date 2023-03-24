99478b1f2a10945c66fbf59da0075825b46e1276
killer-cf <costa.kilder@live.com>
2023-03-24 15:57:48 -0300

    feat: component Heading

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,25 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@killercf-ui/react'
+
+export default {
+  title: 'Typogra/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom Title',
+  },
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    description: {
+      story:
+        'Por padrão o heading sempre será um `h2`, mas podemos alerar isso com a propriedade `as`',
+    },
+  },
+}
